9a154eeb8f6dbf0efc8525afe23dde9d160bd9e1
kamshik28 <dmytriikaminskyi@gmail.com>
2024-09-28 16:50:08 +0200

    Kilos to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,10 +6,18 @@ public class Main {
         double miles = 10;
         double kilometers = milesToKilos(miles);
         System.out.println(miles + " miles is " + kilometers + " kilometers.");
+        kilometers = 16;
+        miles = kilosToMiles(kilometers);
+        System.out.println(kilometers + " kilometers is " + miles + " miles.");
+
 
     }
     public static double milesToKilos(double miles) {
         return miles * 1.60934;
     }
+    public static double kilosToMiles(double kilometers) {
+        return kilometers / 1.60934;
+    }
+
 
 }
