f35540037f7101846e00c02b1f4e80dbafe1546c
kamshik28 <dmytriikaminskyi@gmail.com>
2024-09-28 16:47:39 +0200

    Miles to kilos

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,13 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter App - Version 1.0");
+        double miles = 10;
+        double kilometers = milesToKilos(miles);
+        System.out.println(miles + " miles is " + kilometers + " kilometers.");
+
     }
+    public static double milesToKilos(double miles) {
+        return miles * 1.60934;
+    }
+
 }
